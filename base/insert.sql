947fbe6f42ce97898178c12cfc7d621c0d4c2817
Flo-rencia16 <dalilahandriamiaramanana@gmail.com>
2026-02-09 16:20:44 +0300

    fit insert status

---
+++ base/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO status_echange (libelle) VALUES
+('En cours'),
+('Accepté'),
+('Refusé'),
+('Annulé');
+
+INSERT INTO status_objets (libelle) VALUES
+('Disponible'),
+('En échange'),
+('Indisponible');
